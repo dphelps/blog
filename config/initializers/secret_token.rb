74005406cca1649b59437e0e01f56acfaadf4df5
Daniel C Phelps <dphelps@cmu.edu>
2012-03-26 15:26:15 +0300

    Committed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '1c594b8769b7bb644f6c46b4800ff118f98d5dc1b9d3fae931e8fe576cae892dad18f75c3ce90bf4fb5f2ad62afc14863358e55efdc2ad0d527252d7072137f8'
